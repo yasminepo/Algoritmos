4401e98bd8a09a8e87faed580ab98d72827399a7
yasminepo <ynepomuceno488@gmail.com>
2026-04-23 16:24:37 -0300

    Add files via upload

---
+++ Aluno.py
@@ -0,0 +1,7 @@
+usuário= input("Digite o nome do usuário: ")
+senha= input("Digite a senha: ")
+
+if usuário == "aluno" and senha == "aluno@ifpi":
+    print("Seja bem vindo")
+else:
+    print("Acesso negado")
