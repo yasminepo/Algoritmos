da62f9a3fd1845f90f6697220da8226bc966c1d1
yasminepo <ynepomuceno488@gmail.com>
2026-07-15 12:16:45 -0300

    Update remover_view.py

--- view/remover_view.py
+++ view/remover_view.py
@@ -1,1 +1,23 @@
+import streamlit as st
+from controller import usuario_controller
 
+
+def tela_remover():
+    
+    st.title("🗑️ Remover Usuário")
+
+    login = st.text_input("Login a remover")
+
+    if st.button("Remover"):
+        sucesso, mensagem = usuario_controller.controlador_remover_usuario(login)
+
+        if sucesso:
+            st.success(mensagem)
+        else:
+            st.warning(mensagem)
+
+    st.write("---")
+
+    if st.button("⬅ Voltar ao menu"):
+        st.session_state.tela = "menu"
+        st.rerun()
