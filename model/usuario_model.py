fbd2e0de2bc5e0966cceb01afa8adc913de6ca14
yasminepo <ynepomuceno488@gmail.com>
2026-07-15 12:13:32 -0300

    Update usuario_model.py

--- model/usuario_model.py
+++ model/usuario_model.py
@@ -1,1 +1,37 @@
+rom model.banco_dados import usuarios
 
+def inserir_usuario(login, senha):
+   
+    usuarios[login] = senha
+
+
+def remover_usuario(login):
+   
+    if login in usuarios:
+        del usuarios[login]
+        return True
+    return False
+
+
+def pesquisar_usuario(login):
+  
+    if login in usuarios:
+        return True
+    return False
+
+
+def listar_usuarios():
+   
+    lista_de_logins = []
+
+    for login in usuarios:
+        lista_de_logins.append(login)
+
+    return lista_de_logins
+
+
+def validar_login(login, senha):
+  
+    if login in usuarios and usuarios[login] == senha:
+        return True
+    return False
